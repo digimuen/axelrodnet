4b056b0f6063f9771fb14d98d9744c25ae258014
Johannes Nakayama <johannes.nakayama@rwth-aachen.de>
2020-07-08 15:00:59 +0200

    minor changes

--- experiments.jl
+++ experiments.jl
@@ -55,49 +55,49 @@ for sbf in 0.00:0.01:0.2
 end
 
 # Erdos-Renyi graph
-# for sbf in 0.00:0.01:0.2, p in 0.3:0.1:0.8
-#     run_experiment(
-#         experiment_name="erdosrenyi_sbf" * string(trunc(Int, sbf * 100)) * "_p" * string(trunc(Int, p * 10)),
-#         agentcount=10_000,
-#         n_iter=1_000_000,
-#         nettopology=2,
-#         networkprops=Dict("p" => p),
-#         socialbotfrac=sbf,
-#         rndseed=1,
-#         repcount=3,
-#         export_every_n=10_000,
-#         export_experiment=true
-#     )
-# end
+for sbf in 0.00:0.01:0.2, p in 0.3:0.1:0.8
+    run_experiment(
+        experiment_name="erdosrenyi_sbf" * string(trunc(Int, sbf * 100)) * "_p" * string(trunc(Int, p * 10)),
+        agentcount=10_000,
+        n_iter=1_000_000,
+        nettopology=2,
+        networkprops=Dict("p" => p),
+        socialbotfrac=sbf,
+        rndseed=1,
+        repcount=3,
+        export_every_n=10_000,
+        export_experiment=true
+    )
+end
 
 # Watts-Strogatz graph
-# for sbf in 0.00:0.01:0.2, beta in 0.1:0.1:0.5
-#     run_experiment(
-#         experiment_name="wattsstrogatz_sbf" * string(trunc(Int, sbf * 100)) * "_beta" * string(trunc(Int, beta * 10)),
-#         agentcount=10_000,
-#         n_iter=1_000_000,
-#         nettopology=3,
-#         networkprops=Dict("k" => 10, "beta" => beta),
-#         socialbotfrac=sbf,
-#         rndseed=1,
-#         repcount=3,
-#         export_every_n=10_000,
-#         export_experiment=true
-#     )
-# end
+for sbf in 0.00:0.01:0.2, beta in 0.1:0.1:0.5
+    run_experiment(
+        experiment_name="wattsstrogatz_sbf" * string(trunc(Int, sbf * 100)) * "_beta" * string(trunc(Int, beta * 10)),
+        agentcount=10_000,
+        n_iter=1_000_000,
+        nettopology=3,
+        networkprops=Dict("k" => 10, "beta" => beta),
+        socialbotfrac=sbf,
+        rndseed=1,
+        repcount=3,
+        export_every_n=10_000,
+        export_experiment=true
+    )
+end
 
 # Barabasi-Albert graph
-# for sbf in 0.00:0.01:0.2
-#     run_experiment(
-#         experiment_name="barabasialbert_sbf" * string(trunc(Int, sbf * 100)),
-#         agentcount=10_000,
-#         n_iter=1_000_000,
-#         nettopology=4,
-#         networkprops=Dict("m0" => 10),
-#         socialbotfrac=sbf,
-#         rndseed=1,
-#         repcount=3,
-#         export_every_n=10_000,
-#         export_experiment=true
-#     )
-# end
+for sbf in 0.00:0.01:0.2
+    run_experiment(
+        experiment_name="barabasialbert_sbf" * string(trunc(Int, sbf * 100)),
+        agentcount=10_000,
+        n_iter=1_000_000,
+        nettopology=4,
+        networkprops=Dict("m0" => 10),
+        socialbotfrac=sbf,
+        rndseed=1,
+        repcount=3,
+        export_every_n=10_000,
+        export_experiment=true
+    )
+end
